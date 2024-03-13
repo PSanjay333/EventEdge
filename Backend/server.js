25149f9ac9b79c186b166dadebf5ce56d26d6c9b
PSanjay333 <sanjay.pothuraju11@gmail.com>
2024-03-13 22:28:12 +0530

    Add vercel.json

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,12 @@ mongoDB();
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
-app.use(cors({ origin: "*" }));
+app.use(cors({ 
+  origin: "https://deploy-mern-1whq.vercel.app" ,
+  methods:["POST","GET","PUT"],
+  credentials:true
+
+}));
 
 
 app.get("/", async (req, res) => {
