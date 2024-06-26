4d54f5631164cc948d3c94a4d4dd2881e3ea2412
Sanjay Pothuraju <147619231+PSanjay333@users.noreply.github.com>
2024-06-26 11:36:15 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cors({ 
-  origin: "*" ,
+  origin: "https://event-edge-backend.vercel.app/" ,
 }));
 
 
